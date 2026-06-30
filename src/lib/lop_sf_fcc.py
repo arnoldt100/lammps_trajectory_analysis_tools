8b91a3e6c2cf18644ce08223efc7cd4a760711e8
Tharrington, Arnold <arnoldt@ornl.gov>
2026-06-30 11:34:19 -0400

    Initial indexing of file "".

--- src/lib/lop_sf_fcc.py
+++ src/lib/lop_sf_fcc.py
@@ -1,8 +1,23 @@
 #! /usr/bin/env python3
+""" This module contains the LopSfFcc class definition.
 
+The public members provided by this module are:
+
+    key_lop_sf_fcc : string
+    LopSfFcc : A callable class
+"""
+
+
+""" A key that is uniquely associated with class LopSfFcc.
+
+This key is used by other classes, especially builder classes, to register the
+class LopSfFcc. These callable class each have a unique key or undefined
+behavior may occur.
+"""
 key_lop_sf_fcc = '__lop_sf_fcc__'
 
 class LopSfFcc:
+    """ A callable that calculates a fcc local order parameter. """
     def __init__(self,*args,**kwarfs)->None:
         return
 
