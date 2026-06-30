8762c53ea585e18c9205f01d2b9c8b28067c6cb3
Arnold Tharrington <arnoldt100@hotmail.com>
2026-06-30 04:18:44 -0400

    Added configuration files for machine NimzoIndian.

---
+++ NimzoIndian.env.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# ----------------------------------------------------
+# The absolute path to the package top level directory
+#
+# This file must be sourced in top level of the workspace.
+# ----------------------------------------------------
+export LTAT_TOP_LEVEL="$(pwd)"
+
+# ----------------------------------------------------
+# The absolute path to the directory that contains the build configurations
+#
+# ----------------------------------------------------
+export LTAT_BUILD_CONFIG_ROOT=${LTAT_TOP_LEVEL}/runtime_env_configuration
+
+# ---------------------------------------------------
+# Modify the PATH variable.
+# ---------------------------------------------------
+export PATH="${LTAT_TOP_LEVEL}/src/bin:${PATH}"
+
+# ---------------------------------------------------
+# Modify the PYTHONPATH variable.
+# ---------------------------------------------------
+export PYTHONPATH="$(pwd)/src/lib:${PYTHONPATH}"
+
+# ---------------------------------------------------
+# Set the machine name.
+#
+# ---------------------------------------------------
+export LTAT_MACHINE="NimzoIndian"
+
+# ---------------------------------------------------
+# Ensure that the environmental variable 'LTAT_BUILD_CONFIGURATION'
+# is set and not null and the corresponding build configuration file exists.
+#
+# ---------------------------------------------------
+if [[ ! ${LTAT_BUILD_CONFIGURATION:+LTAT_BUILD_CONFIGURATION} ]]; then
+  echo 'Warning! The environmental variable LTAT_BUILD_CONFIGURATION is not set.'
+  echo 'Please set to a valid value or the various analysis tools will fail.'
+fi
+
+if [[ ! -e "${LTAT_BUILD_CONFIG_ROOT}/${LTAT_BUILD_CONFIGURATION}" ]]; then
+  echo "Warning! The build configuration file ${LTAT_BUILD_CONFIGURATION} doesn't exist."
+  echo "According to the environment variables, the script is looking for the file at:"
+  echo "${LTAT_BUILD_CONFIG_ROOT}/runtime_env_configuration/$LTAT_BUILD_CONFIGURATION"
+  echo 'Please check your build configuration is in the correct directory.'
+fi
+export LTAT_BASH_BUILD_CONFIGURATION="${LTAT_BUILD_CONFIG_ROOT}/${LTAT_BUILD_CONFIGURATION}"
+source ${LTAT_BASH_BUILD_CONFIGURATION}
---
+++ runtime_env_configuration/nimzoindian.runtime.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+# ---------------------------------------------------
+# Ensure that the core runtime file is sourced. This file
+# file sets environmental variables that ar common across
+# machines.
+#
+# ---------------------------------------------------
+source ${LTAT_BUILD_CONFIG_ROOT}/core.env.sh
