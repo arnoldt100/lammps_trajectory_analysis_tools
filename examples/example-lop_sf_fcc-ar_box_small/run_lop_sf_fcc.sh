6e9c56d76d46897457b19bc49bee5503bfec4f52
Arnold Tharrington <arnoldt100@hotmail.com>
2026-07-01 15:32:03 -0400

    Updated example driver script.

--- examples/example-lop_sf_fcc-ar_box_small/run_lop_sf_fcc.sh
+++ examples/example-lop_sf_fcc-ar_box_small/run_lop_sf_fcc.sh
@@ -1,11 +1,5 @@
 #! /usr/bin/env bash
 
-# Name of dcd file.
-dcd_file=argon_box_small.dcd 
-
-# Name of psf file.
-psf_file=argon_box_small.psf
-
 # ----------------------
 # Argon force field settings.
 # The force field parameters are from the following source:
@@ -15,12 +9,39 @@ psf_file=argon_box_small.psf
 # See Table 2 for the Wh[2] force field.
 # In the paper the reparameterized force field for Wh[2] is epsilon is
 # 0.94639 KJ/mol and sigma is 0.33713 nm.
-# 
-# For the units real, distance is in angstroms and energy is in Kcal/mol. 
+#
+# For the LAMMPS units real, distance is in angstroms and energy is in Kcal/mol. 
 # We need to convert nm to angstroms and KJ/mol to Kcal/mol to use Wh[2]
-# force field in this LAMMPS command file.
+# force field in a LAMMPS command file.
 # 
-# The fcc edge length is ~5.196 angstroms
+# The fcc edge length for argon is ~5.196 angstroms for a lj 12-6 potential
+# with sigma of 0.33713 nm.
+#
+# The original DCD file contains 5,849 atoms was split
+# with the command
+#
+#   split -d -b 50M argon_box_small.dcd argon_box_small_dcd_
+#
+# We split the original DCD file to avoid the large file size restriction for git
+# repositories. GitHub sets their restriction to file sizes < 100MB.
+#
+# We reform the original DCD file with the command
+#
+#   cat argon_box_small_dcd_* > ${dcd_file}
+# 
 # ----------------------
 
-uv run  lammps_analysis_tool.py lop_sf_fcc --dcd-file-name argon_box_small.dcd  --psf argon_box_small.psf --edge-length 5.196
+# Name of DCD file.
+dcd_file=argon_box_small.dcd
+
+# Name of PSF file.
+psf_file=argon_box_small.psf
+
+# The edge length in angstroms.
+edge_length=5.196
+
+# Reform the original DCD file.
+cat argon_box_small_dcd_* > ${dcd_file}
+
+# Run the example.
+uv run lammps_analysis_tool.py lop_sf_fcc --dcd-file-name ${dcd_file} --psf ${psf_file} --edge-length ${edge_length}
