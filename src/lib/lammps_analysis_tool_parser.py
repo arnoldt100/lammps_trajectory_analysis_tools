2a560b9a0ddf6fbfe4cc5bc774ec6e18ceeb5b5f
Arnold Tharrington <arnoldt100@hotmail.com>
2026-07-03 17:17:19 -0400

    Latest broken commit.

--- src/lib/lammps_analysis_tool_parser.py
+++ src/lib/lammps_analysis_tool_parser.py
@@ -3,14 +3,20 @@
 """  This module creates the parsers for the command line arguments.
 
 This module's responsibility is to create the top level parser and the subparsers
-for this project. Each analysis tool has a corresponding subparser.
+for this project. Each analysis tool has a corresponding subparser and a  
+corresponding subcommand.
 
-We use a factory or builder pattern where each subparser has a concrete 
-builder that adds the appropriates options, help messages, etc. 
+We use a factory or builder pattern where each subparser/subcommand has a 
+concrete builder that adds the appropriates options, help messages, etc. 
 
+The procedure to add a new subparser is demonstrated with the "lop_sf_fcc"
+subcommand.
+
+
+See the section that adds the LOP Structure FCC order parameter as
+an example.
 """
 
-# Python standard library imports.
 import argparse
 from typing import Any
 from typing import TypeAlias
@@ -23,76 +29,8 @@ from lop_sf_fcc.lop_sf_fcc_cli_parser import CLILopSfFcc
 from lop_sf_fcc.lop_sf_fcc_cli_parser import process_lop_sf_fcc_cli_args
 from lop_sf_fcc.lop_sf_fcc_cli_parser import lop_sf_fcc_subcommand_name
 
-# ----------
-# Public members
-# ----------
-
-""" A type alias that is the union of all subcommand command line interface types. """
+""" Define a type alias that is the union of all subcommand command line interface types. """
 CLI_ID: TypeAlias = CLILopSfFcc
-
-""" A type alias that is the union of all subparser factories. """
-SubparserFactory_ID: TypeAlias = LopSfFccSubparserFactory
-
-def process_command_line_arguments()->CLI_ID:
-    """Processes the command line arguments. """
-
-    my_top_level_parser = argparse.ArgumentParser(prog="lammps_analysis_tool_parser",
-        description="Calculates various physical properties of LAMMPS simulations")
-
-    my_top_level_subparser = my_top_level_parser.add_subparsers(dest="subcommand_name",help="subcommand help")
-
-    # Instatiate a general subparser factory.
-    general_subparser_factory = _GeneralSubparserFactory()
-
-    # Instatiate an empty dict to store functions that
-    # processs the subcommand command line arguments.
-    parse_subcommand_args = {}
-
-    # ----
-    # ---  Do over every subcommand.
-    # ----
-
-    (parse_subcommand_args,general_subparser_factory) = (
-        _process_subcommand_args(parse_subcommand_args,
-                                 my_top_level_subparser,
-                                 general_subparser_factory,
-                                 _lop_sf_fcc_builder_key,
-                                 LopSfFccSubparserFactory,
-                                 process_lop_sf_fcc_cli_args) )
-    # ----
-    # --- End of doing every sucommand 
-    # ----
-
-    #
-    # Now parse the command line args for the subcommand.
-    my_args = my_top_level_parser.parse_args()
-    subcommand_name = my_args.subcommand_name
-    my_CLIArgs : CLI_ID = parse_subcommand_args[subcommand_name](my_top_level_parser)
-    return my_CLIArgs
-
-# ----------
-# Private members
-# ----------
-
-_lop_sf_fcc_builder_key = lop_sf_fcc_subcommand_name()
-
-def _process_subcommand_args (parse_subcommand_args,
-                              my_top_level_subparser,
-                              general_subparser_factory,
-                              registration_key: str,
-                              subparser_factory: SubparserFactory_ID,
-                              func_process_cli_args):
-
-    # Register the builder and the function to process the command line arguments
-    # for the LOP FCC fcc structure factor.
-    general_subparser_factory.register_builder(registration_key,subparser_factory)
-
-    # Invoke the add_subparser method to add the subparser
-    # to the top level parser.
-    general_subparser_factory.add_subparser(registration_key,my_top_level_subparser)
-
-    parse_subcommand_args[registration_key] = func_process_cli_args
-    return (parse_subcommand_args,general_subparser_factory)
 
 
 class _GeneralSubparserFactory:
@@ -112,4 +50,50 @@ class _GeneralSubparserFactory:
         my_builder = builder()
         my_builder(top_level_subparsers,*args,**kwargs)
 
+# ----------
+# This section adds the subparser for the calculating
+# LOP Structure FCC.
+# ----------
+
+# Register the concrete builder LopSfFccSuparserFactory.
+# Each builder key must unique or the undefined behavoir will occur.
+# The buider must be name of the subcommand name specified on the command line.
+_lop_sf_fcc_builder_key = lop_sf_fcc_subcommand_name
+
+def process_command_line_arguments()->CLI_ID:
+    """Processes the command line arguments. """
+
+    my_top_level_parser = argparse.ArgumentParser(prog="lammps_analysis_tool_parser",
+        description="Calculates various physical properties of LAMMPS simulations")
+
+    my_subparsers = my_top_level_parser.add_subparsers(dest="subcommand_name",help="subcommand help")
+
+    # Instatiate a subparser factory.
+    my_subparser_factory = _GeneralSubparserFactory()
+
+    # Register the builder and the function to process the command line arguments
+    # for the LOP FCC fcc structure factor.
+    my_subparser_factory.register_builder(_lop_sf_fcc_builder_key,
+        LopSfFccSubparserFactory)
+
+    parse_subcommand_args = { _lop_sf_fcc_builder_key : process_lop_sf_fcc_cli_args }
+
+    # Invoke the add_subparser method to add the subparser
+    # to the top level parser.
+    my_subparser_factory.add_subparser(_lop_sf_fcc_builder_key,my_subparsers)
+
+    # Now parse the command line args for the subcommand.
+    my_args = my_top_level_parser.parse_args()
+    my_CLIArgs : CLI_ID = parse_subcommand_args[my_args.subcommand_name](my_top_level_parser)
+
+    return my_CLIArgs 
+
+# ----------
+# End of section that adds the subparser for the calculating
+# LOP Structure FCC.
+# ----------
+
+# ----------
+# Add all additional subparsers below this line.
+# ----------
 
