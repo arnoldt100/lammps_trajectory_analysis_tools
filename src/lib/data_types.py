1e319f8a391168b2220d9fbe7526fbe6acdb769c
Arnold Tharrington <arnoldt100@hotmail.com>
2026-07-05 13:35:12 -0400

    New alias typevar added for atom coordinates.

--- src/lib/data_types.py
+++ src/lib/data_types.py
@@ -1,6 +1,10 @@
-from typing import Literal, TypeAlias
+#! /usr/bin/env python3
+from typing import Literal, TypeVar
 import numpy as np
 
 # A C-contiguous N x 3 array of 32-bit floats for atomic positions
-AtomCoordinates: TypeAlias = np.ndarray[tuple[int, Literal], np.dtype[np.float32]]
+type AtomCoordinates16 = np.ndarray[tuple[int, Literal[3]], np.dtype[np.float16]]
+type AtomCoordinates32 = np.ndarray[tuple[int, Literal[3]], np.dtype[np.float32]]
+type AtomCoordinates64 = np.ndarray[tuple[int, Literal[3]], np.dtype[np.float64]]
+AtomCoordinates = TypeVar('AtomCoordinates', AtomCoordinates16, AtomCoordinates32, AtomCoordinates64)
 
