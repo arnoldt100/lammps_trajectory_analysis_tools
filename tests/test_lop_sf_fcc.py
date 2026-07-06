16ec9ac89659a47fc0b927530f858f4ccf4afd4c
Tharrington, Arnold <arnoldt@ornl.gov>
2026-07-06 17:17:20 -0400

    End of workday on RuyLopez.

--- tests/test_lop_sf_fcc.py
+++ tests/test_lop_sf_fcc.py
@@ -12,11 +12,47 @@ from data_types import AtomCoordinates
 
 
 class TestLopSfFcc(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+
+        """ A FCC cubic structure with an edge length equal to 5.19 angstroms"""
+        cls.edge_length = np.float64(5.19)
+
+        cls.neighbor_cutoff = 2.0*np.float64(cls.edge_length)
+
+        cls.atom_coordinates =  cls.edge_length*np.array(
+             [[0.00,  0.00, 0.00],
+              [0.50, 0.50, 0.00],
+              [0.50, 0.00, 0.50],
+              [0.00,  0.50, 0.50],
+              [0.00,  0.00, 1.00],
+              [0.50, 0.50, 1.00],
+              [0.00,  1.00, 0.00],
+              [0.50, 1.00, 0.50],
+              [0.00,  1.00, 1.00],
+              [1.00, 0.00, 0.00],
+              [1.00, 0.50, 0.50],
+              [1.00, 0.00, 1.00],
+              [1.00, 1.00, 0.00],
+              [1.00, 1.00, 1.00]],dtype=np.float64)
+
+        k_0 = np.array([1.00, 0.00, 0.00],dtype=np.float64)
+        k_1 = np.array([0.00, 1.00, 0.00],dtype=np.float64)
+        k_2 = np.array([0.00, 0.00, 1.00],dtype=np.float64)
+        k_3 = np.array([1.00, 1.00, 0.00],dtype=np.float64)
+        k_4 = np.array([1.00, -1.00, 0.00],dtype=np.float64)
+        k_5 = np.array([0.00, 1.00, 1.00],dtype=np.float64)
+        cls.wave_vectors = (2.00/np.pi)*(1.00/cls.edge_length)*(np.array([k_0,k_1,k_2,k_3,k_4,k_5] ,dtype=np.float64))
+
     def test_lop_sf_fcc_single_atom(self):
-        atom_coordinates  : AtomCoordinates = np.zeros((3,1),dtype=np.float64)
-        value = calculate_sf_fcc_order_parameter(atom_coordinates)
+        r_0 : AtomCoordinates = self.atom_coordinates[0,:]
+        print (f"r_0: {r_0}")
+        value = calculate_sf_fcc_order_parameter(self.atom_coordinates)
         self.assertAlmostEqual(value,0.00000,places=3)
 
+    @classmethod
+    def tearDownClass(cls):
+        pass
 # ----------
 # Public members
 # ----------
